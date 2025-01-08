417e27327d90c677206ba94f811585a132a68fd5
Stephen <99619016+dan4737@users.noreply.github.com>
2025-01-07 23:41:35 -0500

    Create script.js

---
+++ Backend/Ver.2/Template/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    var current_fs, next_fs, previous_fs;
+    var opacity;
+    var current = 1;
+    var steps = $("fieldset").length;
+
+    setProgressBar(current);
+
+    $(".next").click(function() {
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        // Add Class Active
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+        // Show the next fieldset
+        next_fs.show();
+        // Hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+                opacity = 1 - now;
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                next_fs.css({'opacity': opacity});
+            },
+            duration: 500
+        });
+        setProgressBar(++current);
+    });
+
+    $(".previous").click(function() {
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        // Remove class active
+        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+        // Show the previous fieldset
+        previous_fs.show();
+        // Hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+                opacity = 1 - now;
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({'opacity': opacity});
+            },
+            duration: 500
+        });
+        setProgressBar(--current);
+    });
+
+    function setProgressBar(curStep) {
+        var percent = (curStep - 1) / (steps - 1) * 100;
+        percent = percent.toFixed();
+        $(".progress-bar")
+            .css("width", percent + "%");
+    }
+
+    // Hide all fieldsets except first on initial load
+    $("fieldset").not(":first").hide();
+});
